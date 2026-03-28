fa7b4633f0d891ad9c295c173488dd578486cb11
hiren.kanzariya01 <kanzariyahiren655@gmail.com>
2026-03-28 13:02:34 +0530

    add code

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(cors({
 
 app.use(express.json({ limit: '10mb' }))
 // app.use(cors())
-
+console.log("MONGO_URI:", process.env.MONGO_URI);
 mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
