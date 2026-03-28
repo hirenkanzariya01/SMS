0873356d49d1a9579bfc2542f5dbac199604efc2
hiren.kanzariya01 <kanzariyahiren655@gmail.com>
2026-03-28 11:14:37 +0530

    change in package.json file

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,11 @@ dotenv.config();
 // app.use(bodyParser.json({ limit: '10mb', extended: true }))
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
+app.use(cors({
+  origin: "https://your-frontend-url.vercel.app",
+  credentials: true
+}));
+
 app.use(express.json({ limit: '10mb' }))
 app.use(cors())
 
